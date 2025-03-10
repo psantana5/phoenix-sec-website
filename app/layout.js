8ecca3f62a2d9533868b924f3d7a96a71994014b
Pau Santana <pausantanapi2@gmail.com>
2025-03-10 16:40:32 +0100

    Added layout.js for Analytics and speed insights

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
