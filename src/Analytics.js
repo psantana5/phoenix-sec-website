be7013eefc01ca4c1dcecf741fd3870cdbed34f9
Pau Santana <127206784+psantana5@users.noreply.github.com>
2025-03-19 17:25:58 +0100

    Update Analytics.js

--- src/Analytics.js
+++ src/Analytics.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-const GA_TRACKING_ID = "G-WLS509JKBY"; // Tu ID de seguimiento
+const GA_TRACKING_ID = "G-WLS509JKBY"; 
 
 const sendPageView = (url) => {
   if (window.gtag) {
